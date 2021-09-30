24a85c0f40a66d5568124efceb00189311051b18
SteveOntheMoon <73540419+SteveontheMoon@users.noreply.github.com>
2021-10-01 00:56:10 +0200

    es 2 done

--- Lab1/esercizio_2.c
+++ Lab1/esercizio_2.c
@@ -1,9 +1,10 @@
+/*Esercizio 2 - lezione laboratorio 1*/
 #include <stdio.h>
 
 int main(){
-    int list[10] = {32, 5, 11, 33, 8};
+    int list[10] = {32, 5, 11, 33, 8, 43, 28, 79, 99, 17};
     int i, j, temp;
-    int n = 5;
+    int n = 10;
 
     for(i = 0; i < n - 1; i++){
         for(j = 0; j < n - 1; j++){
@@ -20,3 +21,6 @@ int main(){
     printf("\n");
 return 0;
 }
+/*Puoi renderlo piu veloce implementando una verifica sul controllo del valore successivo:
+se vede che ne ha 3 successivi in ordine assumi che li abbia gia swappati, rimane il 
+problema che se i 3 numeri sono stati inseriti in ordine l'algoritmo non completa l'ordinamento.*/
--- Lab1/quicksort.c
+++ Lab1/quicksort.c
@@ -1,23 +1,27 @@
 /*Alberto Malagamba - esercizio 2 - lezione laboratorio 1*/
 #include <stdio.h>
 
+void bubblesort(int[]);
 int main(){
     int list[10] = {32, 5, 11, 33, 8, 43, 28, 79, 99, 17};
-    int i, j, temp;
-    int n = 10;
+    bubblesort(list);
+    return 0;
+}
 
-    for(i = 0; i < n - 1; i++){
-        for(j = 0; j < n - 1; j++){
+void bubblesort(int list[]){
+    int i, j, temp; /*j = singolo elemento dell'array. temp = variable per lo swap*/
+    int n = 10; /*Numero totale di elementi nell'array*/
+    for(i = 0; i < n - 1; i++){ /*Ripetuto tante volte quante gli elementi dell'array*/
+        for(j = 0; j < n - 1; j++){ 
             if(list[j] > list[j+1]){
                 temp = list[j];
                 list[j] = list[j+1];
                 list[j+1] = temp;
             }
         }
-    }
     for(i = 0; i < n; i++){
         printf(" %d \0.5t", list[i]);
     }
     printf("\n");
-return 0;
+    }
 }
